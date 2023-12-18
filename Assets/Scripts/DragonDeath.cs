7005160a090ad72480d8ebe1994452eab125aae4
Xentios <xenq@hotmail.com>
2023-12-18 16:18:09 +0300

    pixel perfect cam

--- Assets/Scripts/DragonDeath.cs
+++ Assets/Scripts/DragonDeath.cs
@@ -15,7 +15,7 @@ public class DragonDeath : MonoBehaviour
 
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D2(Collision2D collision)
     {
         Debugger.Log("Collision sqrMagnitude is: " + collision.relativeVelocity.sqrMagnitude, Debugger.PriorityLevel.High);
         ContactPoint2D[] contacts = new ContactPoint2D[collision.contactCount];
@@ -33,6 +33,34 @@ public class DragonDeath : MonoBehaviour
         Debugger.Log("Death Collision sqrMagnitude is: " + collision.relativeVelocity.sqrMagnitude,Debugger.PriorityLevel.MustShown);
         animator.SetTrigger("Death");
         col2D.attachedRigidbody.gravityScale = 1f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+        Vector2 relativeVelocity = collision.relativeVelocity;
+        Vector2 collisionNormal = collision.GetContact(0).normal;
+
+      
+        float dotProduct = Vector2.Dot(relativeVelocity, collisionNormal);
+
+
+        float headOnThreshold = (collision.gameObject.layer == LayerMask.NameToLayer("Ground")) ? 7f : 4f;
+
+        Debug.Log(headOnThreshold);
+        // Check if the dot product is below the threshold
+        if (dotProduct < headOnThreshold)
+        {
+            // The collision is not direct or head-on
+            Debug.Log("Not a head-on collision");
+
+            // Add your custom logic or actions here
+        }
+        else
+        {
+            animator.SetTrigger("Death");
+            col2D.attachedRigidbody.gravityScale = 1f;
+        }
     }
 
     public void AnimationEnding()
--- Assets/Scripts/DragonMovement.cs
+++ Assets/Scripts/DragonMovement.cs
@@ -7,6 +7,11 @@ public class DragonMovement : MonoBehaviour
 {
     [SerializeField]
     private float speed=10;
+    [SerializeField]
+    private float upSpeed = 3;
+    [SerializeField]
+    private float downSpeed = 30;
+
 
     [SerializeField]
     private Transform dragonPivot;
@@ -39,26 +44,41 @@ public class DragonMovement : MonoBehaviour
         actionsAsset.Disable();
     }
 
+    private float timer;
     private void Update()
     {
+       
         if (dragonPivot.rotation != Quaternion.identity)
         {
-            dragonPivot.rotation = Quaternion.Lerp(dragonPivot.rotation, Quaternion.identity,Time.deltaTime);
+            timer += Time.deltaTime;
+
+            if (timer > 1f)
+            {
+               // dragonPivot.rotation = Quaternion.Lerp(dragonPivot.rotation, Quaternion.identity, 0.5f);
+                timer = 0;
+            }
+           
+            //PixelRotate p = dragonPivot.GetChild(0).GetComponent<PixelRotate>();
+            //p.SetRotate(rotation.z - 5);
         }
         
     }
 
     private void PressUp(InputAction.CallbackContext callbackContext )
     {
-        rb2d.AddRelativeForce(Vector2.up * speed);
-       
+        rb2d.AddRelativeForce(Vector2.up * upSpeed * rb2d.velocity.x*2f);
+        
+
+
     }
 
     private void PressDown(InputAction.CallbackContext callbackContext)
     {
-        rb2d.AddRelativeForce(Vector2.down*speed);
-        var rotation = dragonPivot.rotation.eulerAngles;
-        dragonPivot.rotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z - 5);// - 10000f * (float) callbackContext.duration);
+        rb2d.AddRelativeForce(Vector2.down*downSpeed);
+        //var rotation = dragonPivot.rotation.eulerAngles;
+        //dragonPivot.rotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z - 5);// - 10000f * (float) callbackContext.duration);
+        //PixelRotate p = dragonPivot.GetChild(0).GetComponent<PixelRotate>();
+        //p.SetRotate(rotation.z - 5);
     }
 
     private void PressRight(InputAction.CallbackContext callbackContext)
